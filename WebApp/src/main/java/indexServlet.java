21da15cf91a5e565b9941c72c725570fbeae1e56
Haku <89026040+sakanaJA@users.noreply.github.com>
2023-08-06 01:48:16 +0900

    いろいろ

--- WebApp/src/main/java/indexServlet.java
+++ WebApp/src/main/java/indexServlet.java
@@ -1,16 +1,18 @@
 
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class indexServlet
  */
-@WebServlet("/indexServlet")
+@WebServlet("/")
 public class indexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +29,9 @@ public class indexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view/index.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
--- WebApp/src/main/webapp/view/index.jsp
+++ WebApp/src/main/webapp/view/index.jsp
@@ -7,6 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>Test</h1>
+<textarea id="prompt-textarea" tabindex="0" data-id="root" rows="1" placeholder="Send a message" class="m-0 w-full resize-none border-0 bg-transparent p-0 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pr-12 pl-3 md:pl-0" style="max-height: 200px; height: 24px; overflow-y: hidden;">js</textarea>
 
 </body>
+<script>document.getElementById('prompt-textarea').value = 'ここに初期値を入れる';
+</script>
 </html>
